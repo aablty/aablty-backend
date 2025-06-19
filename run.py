33920a6f487cd487df2602eba280091f9cd9376e
aablty <acrouple@yandex.ru>
2025-06-19 05:05:33 +0500

    added: support static

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 import uvicorn
+from fastapi.staticfiles import StaticFiles
+from src.aablty_backend.main import app
+
+# Mount the static files directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     uvicorn.run(
