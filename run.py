860f89c44f7bbbf1b24c117986545298bc3dca8c
aablty <acrouple@yandex.ru>
2025-06-19 17:17:23 +0500

    fix host

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "src.aablty_backend.main:app",
-        host="localhost",
+        host="0.0.0.0",
         port=8000,
         reload=True
     )
